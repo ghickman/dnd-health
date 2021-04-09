83e18ffb89967d21b660a4655468df239f082010
George Hickman <george@ghickman.co.uk>
2021-04-09 13:27:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# D&D Health Dashboard
+
+Scrape and display the party's health stats.
+
+![](https://p198.p4.n0.cdn.getcloudapp.com/items/xQubD8xL/a5d65f74-0779-4875-bac3-f8a197ba0b53.jpg?v=3409f01cff82683707366914f7b13874)
---
+++ script.js
@@ -0,0 +1,47 @@
+const puppeteer = require('puppeteer');
+
+const players = [
+  {name: "Björn", url: "https://www.dndbeyond.com/characters/48002358"},
+  {name: "Einarr", url: "https://www.dndbeyond.com/characters/43201577"},
+  {name: "Gramdoul", url: "https://www.dndbeyond.com/characters/95971"},
+  {name: "Knuckles", url: "https://www.dndbeyond.com/characters/37239100"},
+  {name: "Malcon", url: "https://www.dndbeyond.com/characters/48112517"},
+  {name: "Searos", url: "https://www.dndbeyond.com/characters/37246059"},
+]
+
+const getHP = async (browser, player) => {
+  const page = await browser.newPage()
+
+  // Get the page for the given player
+  await page.goto(player.url)
+
+  // wait for the current health element to be visible
+  await page.waitForSelector('.ct-status-summary-mobile__hp-current', { visible: true })
+
+  const current = await page.$eval('.ct-status-summary-mobile__hp-current', el => el.innerText)
+  const max = await page.$eval('.ct-status-summary-mobile__hp-max', el => el.innerText)
+  const half = Math.floor(parseInt(max) / 2)
+
+  return {...player, current, max, half}
+}
+
+(async () => {
+  const browser = await puppeteer.launch()
+
+  // get the HP from each Player's sheet
+  const results = await Promise.all(
+    players.map(p => { return getHP(browser, p)})
+  )
+
+  // write out name and hp, padded so the colons line up, obvs
+  results.forEach(player => {
+    const name = player.name.padStart(8, " ")
+
+    const current = player.max.padStart(3, " ")
+    const max = player.max.padEnd(3, " ")
+
+    console.log(`${name}: ${current}/${max} (${player.half})`)
+  })
+
+  await browser.close()
+})()
